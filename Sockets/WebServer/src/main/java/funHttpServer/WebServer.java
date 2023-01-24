2c23cf1806bdb0220cdec4e5c4b6699af1aa70d8
Muhammad-Fateen2003 <mfateen@asu.edu>
2023-01-23 21:57:02 -0600

    Add RPC

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -319,43 +319,96 @@ class WebServer {
           }
         }
 
-        } else if (request.contains("madlibs?")) {
+        } else if (request.contains("rrps?")) {
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-          query_pairs = splitQuery(request.replace("madlibs?", ""));
-          String word1 = null;
-          String word2 = null;
+          query_pairs = splitQuery(request.replace("rps?", ""));
+          String player1 = null;
+          String player2 = null;
       
           try {
-              word1 = query_pairs.get("word1");
+              player1 = query_pairs.get("player1");
           } catch (Exception e) {
-              System.out.println("Error: Invalid input for word1. Please enter a valid word.");
+              System.out.println("Error: Invalid input for word1. Please enter either rock, paper or scissors.");
           }
       
           try {
-              word2 = query_pairs.get("word2");
+            player2 = query_pairs.get("player2");
           } catch (Exception e) {
-              System.out.println("Error: Invalid input for word2. Please enter a valid word.");
+              System.out.println("Error: Invalid input for player2. Please enter either rock, paper or scissors.");
           }
       
-          if (word1 != null && word2 != null) {
-              String[] sentences = {"The " + word1 + " jumped over the " + word2 + ".", 
-                                    "The " + word2 + " and the " + word1 + " ran through the forest.", 
-                                    "The " + word1 + " and the " + word2 + " went to the park."};
-              Random rand = new Random();
-              int randomIndex = rand.nextInt(sentences.length);
-              String madlib = sentences[randomIndex];
+          if (player1 != null && player2 != null) {
+            String rpc = null;
+            if(((player1 != "rock") || (player1 != "paper") || (player1 != "scissors")) || ((player2 != "rock") || (player2 != "paper") || (player2 != "scissors"))){
+              rpc = "Please choose either rock, paper, or scissors"
+            }else if(player1 == player2) {
+              rpc = "The Game is a Tie";
+            }else if(player1 == "rock" && player2 == "scissors") {
+              rpc = "Player 1 is the winner"
+            }else if(player1 == "scissors" && player2 == "papper") {
+              rpc = "Player 1 is the winner"
+            }else if(player1 == "papper" && player2 == "rock") {
+              rpc = "Player 1 is the winner"
+            }else if(player2 == "rock" && player1 == "scissors") {
+              rpc = "Player 2 is the winner"
+            }else if(player2 == "scissors" && player1 == "papper") {
+              rpc = "Player 2 is the winner"
+            }else if(player2 == "papper" && player1 == "rock") {
+              rpc = "Player 2 is the winner"
+            }
+              String[] choices = {"rock", "paper", "scissors"};
+              // Random rand = new Random();
+              // int randomIndex = rand.nextInt(sentences.length);
+              // String madlib = sentences[randomIndex];
               builder.append("HTTP/1.1 200 OK\n");
               builder.append("Content-Type: text/html; charset=utf-8\n");
               builder.append("\n");
-              builder.append(madlib);
+              builder.append(rpc);
           } else {
-              System.out.println("Error: One or both of the input words is not valid.");
+              System.out.println("Error: One or both of the input choices is not valid.");
               builder.append("HTTP/1.1 400 Bad Request\n");
               builder.append("Content-Type: text/html; charset=utf-8\n");
               builder.append("\n");
               builder.append("Invalid Parameters");
           }
-      } else {
+      } else if (request.contains("madlibs?")) {
+        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        query_pairs = splitQuery(request.replace("madlibs?", ""));
+        String word1 = null;
+        String word2 = null;
+    
+        try {
+            word1 = query_pairs.get("word1");
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input for word1. Please enter a valid word.");
+        }
+    
+        try {
+            word2 = query_pairs.get("word2");
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input for word2. Please enter a valid word.");
+        }
+    
+        if (word1 != null && word2 != null) {
+            String[] sentences = {"The " + word1 + " jumped over the " + word2 + ".", 
+                                  "The " + word2 + " and the " + word1 + " ran through the forest.", 
+                                  "The " + word1 + " and the " + word2 + " went to the park."};
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(sentences.length);
+            String madlib = sentences[randomIndex];
+            builder.append("HTTP/1.1 200 OK\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append(madlib);
+        } else {
+            System.out.println("Error: One or both of the input words is not valid.");
+            builder.append("HTTP/1.1 400 Bad Request\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Invalid Parameters");
+        }
+    }
+    else {
           // if the request is not recognized at all
 
           builder.append("HTTP/1.1 400 Bad Request\n");
